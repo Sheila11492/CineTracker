5017170223f54d10b0a9bb5d9387143e3a6cf079
Sheila11492 <157987068+Sheila11492@users.noreply.github.com>
2025-11-10 18:44:20 +0100

    Delete SeguimientoDAO.java

--- SeguimientoDAO.java
+++
@@ -1,6 +0,0 @@
-package com.example.cinetracker.data;
-
-public interface SeguimientoDAO {
-    void upsert(int usuarioId, int contenidoId, String estado, Long fechaInicioMs, Long fechaFinMs);
-    String getEstado(int usuarioId, int contenidoId);
-}
