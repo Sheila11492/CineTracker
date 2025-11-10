75c0f7d5f3fe838aac33320ffa040f596c3cafe9
Sheila11492 <157987068+Sheila11492@users.noreply.github.com>
2025-11-10 18:44:01 +0100

    Delete EstadisticaService.java

--- EstadisticaService.java
+++
@@ -1,9 +0,0 @@
-package com.example.cinetracker.data;
-
-import java.util.HashMap;
-
-public interface EstadisticaService {
-    int contarVistosPorTipo(int usuarioId, String tipo);
-    HashMap<String, Integer> contarVistosPorGenero(int usuarioId);
-    int minutosTotalesVistos(int usuarioId);
-}
